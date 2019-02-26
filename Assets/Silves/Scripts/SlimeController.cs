8e5ace29071c69b9b1b3209862226e913d96d963
skiress <yulia3627@gmail.com>
2019-02-26 14:03:12 +0100

    kl

--- Assets/Silves/Scripts/SlimeController.cs
+++ Assets/Silves/Scripts/SlimeController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeController : MonoBehaviour
+public class SlimePrueba : MonoBehaviour
 {
     public float speed;
     public float jumpPower;
