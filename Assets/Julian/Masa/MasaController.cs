eded77ac24af3a444f0278fc93b5eba55797aff3
skiress <yulia3627@gmail.com>
2019-02-26 11:42:53 +0100

    Disparo

---
+++ Assets/Julian/Masa/MasaController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasaController : MonoBehaviour
+{
+
+    public float mass = 100;
+    public int lostMassPerSecond = 50;
+    public float timer = 1;
+    public int gainedMass;
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = mass;
+        slider.value = mass;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mass = mass - (lostMassPerSecond * Time.deltaTime);
+            slider.value = mass;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            mass += gainedMass;
+            slider.value = mass;
+        }
+
+        /*
+        if (Input.GetMouseButton(0))
+        {
+
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                mass = mass - lostMassPerSecond;
+                slider.value = mass;
+                print("ya" + mass);
+                timer = 1;
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            timer = 1;
+        }
+
+        */
+    }
+}
